7a2ae06a661c4b150024916fd6d4b8972c3ab8a1
rx80 <everx80@gmail.com>
2014-11-12 20:16:24 +0100

    Add docstrings

--- core/dfraw.py
+++ core/dfraw.py
@@ -24,22 +24,49 @@ class Raw:
 
     @staticmethod
     def open(path, mode):
+        """
+        Opens a raw file at <path> with mode <mode> and returns a stream
+        :param str path: Path to raw file
+        :param mode: File mode
+        :return io.TextIOWrapper:
+        """
         return io.open(path, mode, encoding='cp437', errors='replace')
 
     @classmethod
     def read(cls, path):
+        """
+        Returns text of raw file at <path>
+        :param str path:
+        :return str:
+        """
         with cls.open(path, 'rt') as fd:
             return fd.read()
 
     @classmethod
     def write(cls, path, text):
+        """
+        Writes <text> to a raw file at <path>
+        :param str path:
+        :param str text:
+        :return:
+        """
         with cls.open(path, 'wt') as fd:
             return fd.write(text)
 
     def save(self):
+        """
+        Writes current raw file
+        :return int: bytes written
+        """
         return self.write(self.path, self.text)
 
     def toggle(self, field, enabled):
+        """
+        Toggles (disables/enables) a <field> in the raw file
+        :param str field: Name of the field
+        :param bool enabled:
+        :return:
+        """
         self.modified = True
         if enabled:
             replace_from, replace_to = self._option_enable
@@ -51,6 +78,12 @@ class Raw:
             replace_to.format(field))
 
     def set_value(self, field, value):
+        """
+        Sets the value of <field> to <value>
+        :param str field:
+        :param str value:
+        :return:
+        """
         self.modified = True
         self.text = re.sub(
             r'\[{0}:(.+?)\]'.format(field),
