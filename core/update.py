619a549925b7a134a2f3c8938dda4dd2664897f2
Peridexis Errant <PeridexisErrant@gmail.com>
2015-01-14 23:52:16 +1100

    Add more update parameters, and auto-fill update params from DFFD number.
    See README changes for detail on this.
    
    --HG--
    branch : easy-updating

--- core/update.py
+++ core/update.py
@@ -28,6 +28,7 @@ def check_update():
     if lnp.userconfig.get_number('updateDays') == -1:
         return
     if lnp.userconfig.get_number('nextUpdate') < time.time():
+        simple_dffd_config()
         t = threading.Thread(target=perform_update_check)
         t.daemon = True
         t.start()
@@ -43,6 +44,10 @@ def perform_update_check():
         new_version = re.search(
             lnp.config.get_string('updates/versionRegex'),
             version_text).group(1)
+        if not lnp.config.get_string('updates/packVersion'):
+            lnp.config['updates/packVersion'] = new_version
+            lnp.config.save_data()
+            return
         if new_version != lnp.config.get_string('updates/packVersion'):
             lnp.new_version = new_version
             lnp.ui.on_update_available()
@@ -70,3 +75,25 @@ def download_df_baseline():
     url = 'http://www.bay12games.com/dwarves/' + filename
     target = os.path.join(paths.get('baselines'), filename)
     download.download('baselines', url, target)
+
+def simple_dffd_config():
+    """Reduces the configuration required by maintainers using DFFD.
+    Values are generated and saved from known URLs and the 'dffdID' field."""
+    dffd_num = lnp.config.get_number('updates/dffdID')
+    if dffd_num and not lnp.config.get_string('updates/checkURL'):
+        lnp.config['updates/checkURL'] = ('http://dffd.wimbli.com/file_'
+                                          'version.php?id=' + dffd_num)
+        lnp.config.save_data()
+    if dffd_num and not lnp.config.get_string('updates/versionRegex'):
+        lnp.config['updates/versionRegex'] = 'Version: (.+)'
+        lnp.config.save_data()
+    if dffd_num and lnp.config.get_string('updates/downloadURL'):
+        lnp.config['updates/downloadURL'] = ('http://dffd.wimbli.com/file'
+                                             '.php?id=' + dffd_num)            
+        lnp.config.save_data()
+    if dffd_num and lnp.config.get_string('updates/directURL'):
+        # TODO: improve pack name handling; this works but isn't great
+        fname = 'new_pack.zip'
+        lnp.config['updates/directURL'] = ('http://dffd.wimbli.com/download.'
+                                           'php?id=' + dffd_num + '&f=' + fname)
+        lnp.config.save_data()
