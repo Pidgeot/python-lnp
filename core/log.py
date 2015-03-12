c7c8434509777a95e526b6ac7b47761757d773f1
Michael Madsen <michael@birdiesoft.dk>
2015-03-12 16:19:11 +0100

    Support prefixes in log class (see #70) + minor refactoring

--- core/log.py
+++ core/log.py
@@ -18,20 +18,12 @@ class Log(object):
     """Logging class."""
     def __init__(self):
         """Constructor for Log. Sets the maximum logging level to WARNING."""
-        #pylint: disable=global-statement
-        global _log
-        if not _log:
-            _log = self
         self.max_level = WARNING
         self.output_err = False
         self.output_out = False
         self.level_stack = []
         self.lines = []
-
-    @staticmethod
-    def get():
-        """Returns the active Log instance."""
-        return _log
+        self.prefixes = []
 
     def push_level(self, level):
         """Temporarily changes the logging level to <level>. Call pop_level to
@@ -45,11 +37,29 @@ class Log(object):
         try:
             self.max_level = self.level_stack.pop()
         except IndexError:
-            self.w('Tried to pop logging level, but stack empty', stack=True)
+            self.e('Tried to pop logging level, but stack empty', stack=True)
 
     def set_level(self, level):
         """Sets the maximum logging level to <level>."""
         self.max_level = level
+
+    def push_prefix(self, prefix):
+        """Adds a prefix to future log messages. Old prefixes will appear before
+        new prefixes."""
+        self.prefixes.append(prefix)
+
+    def pop_prefix(self):
+        """Removes the most recently added prefix from future log messages."""
+        try:
+            self.prefixes.pop()
+        except IndexError:
+            self.e('Tried to pop logging prefix, but stack empty', stack=True)
+
+    def __get_prefixes(self):
+        """Returns a string containing the prefixes for this log message."""
+        if not self.prefixes:
+            return ''
+        return ': '.join(self.prefixes+[''])
 
     def log(self, log_level, message, **kwargs):
         """Logs a message if the current logging level includes messages at
@@ -67,7 +77,7 @@ class Log(object):
                 If True, logs a stack trace."""
         if log_level < self.max_level:
             return
-        p = self.__get_level_string(log_level)
+        p = self.__get_level_string(log_level) + self.__get_prefixes()
         self.__write(p + str(message) + "\n")
         if kwargs.get('stack', False):
             for l in traceback.format_stack():
@@ -111,19 +121,25 @@ class Log(object):
         self.lines.append(text)
 
 
+def get():
+    """Returns the default Log instance."""
+    return _log
+
 # prepare the default instance
-Log()
+_log = Log()
 # Output to error log on the default instance
-Log.get().output_err = True
+_log.output_err = True
 
 # expose the methods targeting the default instance on the module itself
-push_level = Log.get().push_level
-pop_level = Log.get().pop_level
-set_level = Log.get().set_level
-log = Log.get().log
-d = Log.get().d
-e = Log.get().e
-i = Log.get().i
-v = Log.get().v
-w = Log.get().w
-get_lines = Log.get().get_lines
+push_level = _log.push_level
+pop_level = _log.pop_level
+set_level = _log.set_level
+log = _log.log
+d = _log.d
+e = _log.e
+i = _log.i
+v = _log.v
+w = _log.w
+get_lines = _log.get_lines
+push_prefix = _log.push_prefix
+pop_prefix = _log.pop_prefix
