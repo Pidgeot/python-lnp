466f04a65bd1cc0e699e1f09939ca55b33b93383
Michael Madsen <michael@birdiesoft.dk>
2015-12-01 23:54:16 +0100

    Fix GridLayouter so it respects the colspan desired by controls as a minimum

--- tkgui/layout.py
+++ tkgui/layout.py
@@ -46,7 +46,9 @@ class GridLayouter(object):
     def layout(self):
         """Applies layout to the added controls."""
         cells_used = 0
+        max_index = len(self.controls) - 1
         for i, c in enumerate(self.controls):
+            c = list(c)
             while True:
                 row = cells_used // self.cols
                 col = cells_used % self.cols
@@ -57,6 +59,17 @@ class GridLayouter(object):
             padx = 0 if col == 0 else (self.pad[0], 0)
             pady = 0 if row == 0 else (self.pad[1], 0)
 
+            if ((i == max_index and col != self.cols - 1) or (
+                    i < max_index and
+                    col + c[1] + self.controls[i+1][1] > self.cols)):
+                # Pad colspan if last control, or next control won't fit
+                colspan = self.cols - col
+                for n in range(col + 1, self.cols):
+                    if (row, n) in self.used:
+                        colspan = n - col
+                        break
+                c[1] = colspan
+
             c[0].grid(
                 row=row, column=col, sticky="nsew", columnspan=c[1], padx=padx,
                 pady=pady, **c[2])
@@ -64,10 +77,3 @@ class GridLayouter(object):
                 for n in range(1, c[2]['rowspan']+1):
                     self.used.append((row+n, col))
             cells_used += c[1]
-            if i == len(self.controls) - 1 and col != self.cols - 1:
-                colspan = self.cols - col
-                for n in range(col + 1, self.cols):
-                    if (row, n) in self.used:
-                        colspan = n - col
-                        break
-                c[0].grid(columnspan=colspan)
