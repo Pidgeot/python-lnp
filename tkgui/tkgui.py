b2a78119211045d8078e741ecc23f3eda354d3c5
Michael Madsen <michael@birdiesoft.dk>
2016-06-22 02:30:26 +0200

    Fix PyLint errors

--- tkgui/tkgui.py
+++ tkgui/tkgui.py
@@ -560,8 +560,8 @@ class TkGui(object):
         messagebox.showinfo(
             title='About', message="PyLNP "+VERSION +" - Lazy Newb Pack Python "
             "Edition\n\nPort by Pidgeot\nContributions by PeridexisErrant, "
-            "rx80, dricus, James Morgensen, jecowa\n\nOriginal program: LucasUP, "
-            "TolyK/aTolyK")
+            "rx80, dricus, James Morgensen, jecowa\n\n"
+            "Original program: LucasUP, TolyK/aTolyK")
 
     @staticmethod
     def cycle_option(field):
